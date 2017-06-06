b21523be10d65d3362f203c9bbbdb9ecc1f9271d
Jerry Lau <jerryzlau@gmail.com>
2017-06-05 17:35:40 -0700

    implemented rpn calculator

--- lib/00_rpn_calculator.rb
+++ lib/00_rpn_calculator.rb
@@ -1,3 +1,71 @@
+require "byebug"
+
 class RPNCalculator
   # TODO: your code goes here!
+
+  def initialize
+    @array = []
+  end
+
+  def value
+    @array.last
+  end
+
+  def push(num)
+    @array.push(num)
+  end
+
+  def plus
+    raise ("calculator is empty") if @array.size < 2
+    @array << @array.pop + @array.pop
+  end
+
+  def minus
+    raise ("calculator is empty") if @array.size < 2
+    first = @array.pop
+    second = @array.pop
+    @array << second - first
+  end
+
+  def divide
+    raise ("calculator is empty") if @array.size < 2
+    first = @array.pop.to_f
+    second = @array.pop.to_f
+    @array << second / first
+  end
+
+  def times
+    raise ("calculator is empty") if @array.size < 2
+    @array << @array.pop * @array.pop
+  end
+
+  def tokens(string)
+    numbers = ("0".."9")
+    string.split.map do |el|
+      numbers.include?(el) ? el.to_i : el.to_sym
+    end
+  end
+
+  def evaluate(string)
+    debugger
+    tokens(string).each do |el|
+      if el == :+
+        self.plus
+      elsif el == :-
+        self.minus
+      elsif el == :*
+        self.times
+      elsif el == :/
+        self.divide
+      else
+        @array.push(el)
+      end
+    end
+    self.value
+  end
+
+
 end
+
+a = RPNCalculator.new
+a.evaluate("1 2 3 * + 4 5 - /")
